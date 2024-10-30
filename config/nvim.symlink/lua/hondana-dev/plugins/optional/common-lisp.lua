48deb341e533e28b7317aed41ae719007245494b
martialboniou <martial@gmx.com>
2024-10-30 02:57:23 +0100

    🐛 FIX: nvim: missing line in gitignore
    `plugins.optional` should only be seen from the Fennel side of this
    setup; the Lua side has `plugins.unchecked` only

--- config/nvim.symlink/lua/hondana-dev/plugins/optional/common-lisp.lua
+++
@@ -1,5 +0,0 @@
--- :fennel:1696020727
-local function _1_()
-  return vim.cmd("call nvlime#plugin#Setup()")
-end
-return {"monkoose/nvlime", ft = "lisp", config = _1_, dependencies = "monkoose/parsley"}
