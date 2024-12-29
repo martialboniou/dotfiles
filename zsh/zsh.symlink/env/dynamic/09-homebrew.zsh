486104d252515d663034d3bcb65509cfe3fecc28
martialboniou <martial@gmx.com>
2024-12-29 20:35:32 +0100

    🐛 FIX: zsh: homebrew dynamic zshenv vars

--- zsh/zsh.symlink/env/dynamic/09-homebrew.zsh
+++ zsh/zsh.symlink/env/dynamic/09-homebrew.zsh
@@ -1,3 +1,14 @@
-#eval $(/opt/homebrew/bin/brew shellenv) #TODO: bug 2021-09-04
 export HOMEBREW_NO_ENV_HINTS=
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+# export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+() {
+  eval $(/opt/homebrew/bin/brew shellenv)
+  if [[ -r "$(brew --prefix llvm)" ]]; then
+    export PATH="$(brew --prefix llvm):$PATH"
+    # append a specific version to the $path:
+    #   `export PATH="$(brew --prefix llvm@18):$PATH"`
+    # ensure llvm is linked (it should already be linked):
+    #   `brew link llvm --force`
+    # don't do the following:
+    #   `cd /opt/homebrew/opt && rm llvm && ln -s ../Cellar/llvm@18/18.1.8 llvm`
+  fi
+}
